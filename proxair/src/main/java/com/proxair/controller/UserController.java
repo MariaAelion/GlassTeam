71e6409d15e324000cf7c922535b9f91bc6f3a5a
Loucas31 <lucasdubois.work@gmail.com>
2019-01-31 03:14:58 +0100

    Coucou

--- proxair/src/main/java/com/proxair/controller/UserController.java
+++ proxair/src/main/java/com/proxair/controller/UserController.java
@@ -8,7 +8,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
