4e73904381ed444031bb38f7aa97bf00b06749c2
maximebn <maximebn@posteo.ru>
2019-01-31 13:33:05 +0100

    Dernière à version à jour

--- proxair/src/main/java/com/proxair/exception/NotFoundException.java
+++ proxair/src/main/java/com/proxair/exception/NotFoundException.java
@@ -1,6 +1,5 @@
 package com.proxair.exception;
 
-<<<<<<< HEAD
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
@@ -15,22 +14,3 @@ public class NotFoundException extends RuntimeException {
 		super(msg);
 	}
 }
-=======
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
-public class NotFoundException extends RuntimeException {
-
-	private static final long serialVersionUID = -1749672979746392283L;
-	
-	public NotFoundException() {
-	}
-
-	public NotFoundException(String msg) {
-		super(msg);
-	}
-
-}
->>>>>>> refs/remotes/origin/master
